81d9e93e625c1f31e8d7aaaadcdca02fa7757fc4
teresa2905 <s345368@oslomet.no>
2022-08-31 17:49:03 +0200

    petra kan telle til tjue

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
+        System.out.println("Teller til ti");
         for (int i=1; i<=10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Petra teller til tjue");
+        for (int i=11; i<=20; i++) {
             System.out.println(i);
         }
     }
